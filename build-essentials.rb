beb7144a24fe27c434c77c0601d528964f644231
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-27 17:16:44 +1100

    Added packages, and moved packages from build-essentials into the main packages list.

--- build-essentials.rb
+++ build-essentials.rb
@@ -1,19 +1,3 @@
 dep('build essential installed') {
-  requires [
-               'build-essential',
-               'binutils-doc.managed',
-               'autoconf.managed',
-               'flex.managed',
-               'bison.managed'
-           ]
+  requires %w(build-essential binutils-doc.managed autoconf.managed flex.managed bison.managed)
 }
-
-dep('binutils-doc.managed') {
-  provides []
-}
-
-dep('autoconf.managed') {}
-
-dep('flex.managed') {}
-
-dep('bison.managed') {}
