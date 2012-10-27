9cb68745abae9219df25f6f395501e1b41b1a470
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-27 17:39:02 +1100

    Updated source path for nodejs

--- node.rb
+++ node.rb
@@ -4,6 +4,6 @@ dep('nodejs installed') {
 
 dep 'nodejs.src', :version do
   version.default!('0.8.14')
-  source "http://nodejs.org/dist/node-v#{version}.tar.gz"
+  source "http://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz"
   provides 'node', 'node-waf'
 end
