e0d54f1056d38ada33f592a1e66196645459cb60
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-28 18:21:08 +1100

    Updated sqlite

--- packages.rb
+++ packages.rb
@@ -4,9 +4,7 @@ dep 'bundler.gem' do
   provides 'bundle'
 end
 dep 'libsqlite3-dev.managed' do
-  installs {
-    via :apt, 'sqlite3.managed', 'libsqlite3-dev.managed'
-  }
+  provides []
 end
 dep 'sqlite3.managed'
 dep 'libzdb.src', :version do
