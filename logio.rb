05dcea593f93d508801558a01b5bcc7b0238fa16
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-28 23:20:17 +1100

    Added logio

---
+++ logio.rb
@@ -0,0 +1,14 @@
+dep 'logio', :install_prefix do
+  install_prefix.default!('/usr/local')
+  
+  requires 'npm installed'
+
+  met? do
+    "#{install_prefix}/log.io".p.exists?
+  end
+
+  meet do
+    shell 'npm config set unsafe-perm true'
+    shell "npm install -g --prefix=#{install_prefix} log.io"
+  end
+end
