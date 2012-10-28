1578c1efee1842ee3728987cd61afa2205960f2f
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-28 12:59:51 +1100

    Updated default to default! in monit, and removed need to pass home_dir_base through from bootstrap

--- lexim.rb
+++ lexim.rb
@@ -1,11 +1,11 @@
-dep 'bootstrap lexim', :username, :home_dir_base, :key do
+dep 'bootstrap lexim', :username, :key do
   setup do
     unmeetable! "This dep has to be run as root." unless shell('whoami') == 'root'
   end
 
   requires [
     'lexim:system'.with(:host_name => shell('hostname -f')),
-    'lexim:user setup for provisioning'.with(:username => username, :key => key, :home_dir_base => home_dir_base),
+    'lexim:user setup for provisioning'.with(:username => username, :key => key),
     'lexim:core dependencies',
     'lexim:build essential installed',
     'lexim:nodejs installed',
