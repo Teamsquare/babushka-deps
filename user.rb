43b7790a15c6d40107f8fa3c9b5f3a15f46b5405
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-28 13:00:54 +1100

    Fixed typo in variable default

--- user.rb
+++ user.rb
@@ -12,7 +12,7 @@ dep 'dot files', :username, :github_user, :repo do
 end
 
 dep 'user setup for provisioning', :username, :key, :home_dir_base do
-  hostname_dir_base.default!('/home')
+  home_dir_base.default!('/home')
   requires [
                'user exists'.with(:username => username, :home_dir_base => home_dir_base),
                'passwordless ssh logins'.with(username, key),
