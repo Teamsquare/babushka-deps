feb712975c0f0b57a9bf6d18853d7242b582bb39
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-28 22:45:39 +1100

    Updated npm

--- npm.rb
+++ npm.rb
@@ -1,9 +1,11 @@
 dep 'npm installed' do
   requires 'nodejs installed'
 
+  met? do
+    'npm'.p.exists?
+  end
+  
   meet do
     shell 'curl https://npmjs.org/install.sh | sudo sh'
   end
-  
-  provides 'npm'
 end
