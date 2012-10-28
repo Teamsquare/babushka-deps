eff341a20d7566942c91054a5c94520bffaf0e3d
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-28 17:06:01 +1100

    Updated prefix to install_prefix

--- mmonit.rb
+++ mmonit.rb
@@ -1,6 +1,6 @@
-dep 'mmonit.running', :version, :prefix do
+dep 'mmonit.running', :version, :install_prefix do
   version.default!('2.4')
-  prefix.default!('/usr/local')
+  install_prefix.default!('/usr/local')
 
   requires %w(mmonit.src mmonit.startable)
 
@@ -17,7 +17,7 @@ dep 'mmonit.running', :version, :prefix do
   end
 end
 
-dep 'mmonit.src', :version, :prefix do
+dep 'mmonit.src', :version, :install_prefix do
   setup do
     must_be_root
   end
@@ -25,27 +25,27 @@ dep 'mmonit.src', :version, :prefix do
   requires 'user and group exist'.with(:user => 'mmonit')
 
   version.default!('2.4')
-  prefix.default!('/usr/local')
+  install_prefix.default!('/usr/local')
 
   met? do
-    "#{prefix}/mmonit-#{version}/bin/mmonit".p.exists?
+    "#{install_prefix}/mmonit-#{version}/bin/mmonit".p.exists?
   end
 
   meet do
     tar_file = "mmonit-#{version}-linux-x64.tar.gz"
     shell "wget http://mmonit.com/dist/#{tar_file} /tmp/#{tar_file}"
-    shell "tar xvf /tmp/#{tar_file} -C #{prefix}"
+    shell "tar xvf /tmp/#{tar_file} -C #{install_prefix}"
     shell "rm /tmp/#{tar_file}"
   end
 end
 
-dep 'mmonit.startable', :version, :prefix do
+dep 'mmonit.startable', :version, :install_prefix do
   setup do
     must_be_root
   end
 
   version.default!('2.4')
-  prefix.default!('/usr/local')
+  install_prefix.default!('/usr/local')
 
   met? { "/etc/monit/monitrc".p.exists? }
   meet do
