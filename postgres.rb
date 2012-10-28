d5bd1e5e5179a87e867187094bf53a7f48240044
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-28 23:12:59 +1100

    Added postgres

--- postgres.rb
+++ postgres.rb
@@ -1,7 +1,11 @@
 dep 'pgclient.installed' do
-  requires 'libpq-dev.managed'
+  requires 'libpq-dev.managed', 'postgresql-client.managed'
 end
 
 dep 'libpq-dev.managed' do
+  provides []
+end
+
+dep 'postgresql-client.managed' do
   provides 'pg_control'
 end
