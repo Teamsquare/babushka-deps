249d4cb3e7307a0b8811a1699469b8975a99f68c
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-29 11:39:36 +1100

    Added unicorn startup script

--- unicorn.rb
+++ unicorn.rb
@@ -7,8 +7,9 @@ dep 'unicorn.startable' do
     must_be_root
   end
 
-  met? { "/etc/init.d/unicorn".p.exists? && "/etc/monit/conf.d/unicorn_master.monitrc".p.exists? && "/etc/monit/conf.d/unicorn_worker.monitrc".p.exists? }
+  met? { "/etc/init/unicorn.conf".p.exists? && "/etc/init.d/unicorn".p.exists? && "/etc/monit/conf.d/unicorn_master.monitrc".p.exists? && "/etc/monit/conf.d/unicorn_worker.monitrc".p.exists? }
   meet do
+    render_erb 'unicorn/unicorn.init.conf.erb', :to => '/etc/init/unicorn.conf', :perms => '755', :sudo => true
     render_erb 'unicorn/unicorn.init.d.erb', :to => '/etc/init.d/unicorn', :perms => '755', :sudo => true
     render_erb "monit/unicorn_master.monitrc.erb", :to => "/etc/monit/conf.d/unicorn_master.monitrc", :perms => 700
     render_erb "monit/unicorn_worker.monitrc.erb", :to => "/etc/monit/conf.d/unicorn_worker.monitrc", :perms => 700
