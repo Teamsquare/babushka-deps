95a2a86477e9ff368cf79c615dae16c9e45b7366
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-10-29 11:33:42 +1100

    Added answers for java license

--- java.rb
+++ java.rb
@@ -1,7 +1,17 @@
 dep 'jre' do
-  requires 'jre.ppa', 'oracle-java7-installer.managed'
+  requires 'jre.ppa', 'agreed to java license' 'oracle-java7-installer.managed'
 end
 
+dep 'agreed to java license' do
+  met? do
+    '~/java-license-accepted'.p.exists?
+  end
+  meet {
+    shell "echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections"
+    shell "echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections"
+    shell 'touch ~/java-license-accepted'
+  }
+end
 dep 'jre.ppa' do
   met? {
     "/etc/apt/sources.list.d/webupd8team-java-precise.list".p.exist?
