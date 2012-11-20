ce1264ac165e8f40517309eabbf24eea9710ae5b
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-11-20 17:30:29 +1100

    Updated JIRA to use an application specific user.

--- jira.rb
+++ jira.rb
@@ -1,12 +1,31 @@
-dep 'jira.installed', :version, :install_prefix, :home_directory do
+dep 'jira.installed', :version, :install_prefix, :home_directory, :jira_user do
   version.default!('5.2')
   install_prefix.default!('/usr/local')
   home_directory.default!('/etc/jira')
+  jira_user.default!('jira')
 
   requires [
                'jre',
+               'jira.user'.with(username_name),
                'jira'.with(version, install_prefix, home_directory),
+               'jira.permissions'
            ]
+end
+
+dep 'jira.user', :username_name do
+  setup do
+    must_be_root
+  end
+
+  met? {
+    '/etc/passwd'.p.grep(/^#{username_name}\:/) and
+        '/etc/group'.p.grep(/^#{username_name}\:/)
+  }
+
+  meet {
+    shell "groupadd #{username_name}"
+    shell "useradd --create-home --comment 'Account for running JIRA' -g #{username_name} -s /bin/bash #{username_name}"
+  }
 end
 
 dep 'jira', :version, :install_prefix, :home_directory do
@@ -27,4 +46,8 @@ dep 'jira', :version, :install_prefix, :home_directory do
     shell "mkdir -p #{home_directory}"
     shell "echo 'jira.home=#{home_directory}' > #{install_prefix}/atlassian-jira/webapp/WEB-INF/classes/jira-application.properties"
   end
+end
+
+dep 'jira.permissions' do
+
 end
