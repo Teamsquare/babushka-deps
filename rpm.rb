2b767bd277fbabe76e14b5b1391657109df9e581
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-12-10 17:13:13 +1100

    Updated rpm monitoring to work on CentOS

--- rpm.rb
+++ rpm.rb
@@ -4,29 +4,35 @@ dep 'rpm monitoring', :new_relic_license do
   end
 
   requires [
-    'new relic apt-get source registered',
-    'new relic public key installed',
-    'newrelic-sysmond.managed',
-    'new relic configured'.with(new_relic_license),
-    'new relic started'
-  ]
+     'new relic package source registered',
+     'new relic public key installed',
+     'newrelic-sysmond.managed',
+     'new relic configured'.with(new_relic_license),
+     'new relic started'
+   ]
 end
 
-dep 'new relic apt-get source registered' do
+dep 'new relic package source registered' do
   setup do
     must_be_root
   end
 
-  met? do
-    "/etc/apt/sources.list.d/newrelic.list".p.exists?
+  on :apt do
+    met? do
+      shell('rpm -q newrelic').p.grep(/is not installed/)
+    end
+
+    meet do
+      shell('rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm')
+    end
   end
 
-  meet do
-    shell('wget -O /etc/apt/sources.list.d/newrelic.list http://download.newrelic.com/debian/newrelic.list')
+  on :yum do
+
   end
 end
 
-dep 'new relic public key installed' do
+dep 'new relic public key installed', :for => [:ubuntu, :debian] do
   setup do
     must_be_root
   end
@@ -63,6 +69,7 @@ end
 dep 'newrelic-sysmond.managed' do
   installs {
     via :apt, 'newrelic-sysmond'
+    via :yum, 'newrelic-sysmond'
   }
 
   provides 'nrsysmond-config'
