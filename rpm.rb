89436dd3c254cced72f4eb44a27d838c46c1fb6b
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-12-10 17:14:42 +1100

    Fixed logic bug in checking for RPM libraries

--- rpm.rb
+++ rpm.rb
@@ -19,7 +19,7 @@ dep 'new relic package source registered' do
 
   on :apt do
     met? do
-      shell('rpm -q newrelic').p.grep(/is not installed/)
+      !shell('rpm -q newrelic').p.grep(/is not installed/)
     end
 
     meet do
