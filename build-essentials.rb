8f1b754373c2d8b1c17278601a06159f2b95a96f
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-12-11 13:23:14 +1100

    Changed the check for whether development tools is installed or not

--- build-essentials.rb
+++ build-essentials.rb
@@ -8,7 +8,7 @@ end
 
 dep 'centos build essential installed' do
   met? do
-    !(shell('yum -q grouplist "Development Tools"').empty?)
+    !(shell('yum -q grouplist "Development Tools"').grep(/Installed Groups/).empty?)
   end
 
   meet do
