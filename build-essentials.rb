ea65b82db3e845cd91edd15bf3b7ed1fa3af661c
Michael Shimmins <michael.shimmins@lexim.com.au>
2012-12-11 12:03:35 +1100

    Made a built essentials for cents using the Developer Tools group

--- build-essentials.rb
+++ build-essentials.rb
@@ -1,3 +1,13 @@
-dep('build essential installed') {
+dep 'build essential installed', :for => [:ubuntu, :debian] do
   requires %w(build-essential binutils-doc.managed autoconf.managed flex.managed bison.managed)
-}
+end
+
+dep 'build essential installed', :for => [:centos] do
+  met? do
+    !(shell('yum -q grouplist "Development Tools"').empty?)
+  end
+
+  meet do
+    shell('yum groupinstall "Development Tools"')
+  end
+end
