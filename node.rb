ae8a2b5e9e6835cd8c20253fbe3b84bf25a92010
Michael Shimmins <michael@teamsquare.co>
2016-01-05 15:18:22 +1100

    Upgraded node

--- node.rb
+++ node.rb
@@ -1,5 +1,5 @@
 dep 'nodejs.src', :version do
-  version.default!('0.10.5')
-  source "http://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz"
+  version.default!('4.2.4')
+  source "https://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz"
   provides 'node'
 end
