cba28eeea30e904ddd5cb1154603e3a91b9d42fd
Michael Shimmins <michael@teamsquare.co>
2016-01-07 16:51:27 +1100

    Still need the PPA in trusty :/

--- postgres.rb
+++ postgres.rb
@@ -1,3 +1,22 @@
+dep 'pg.ppa' do
+	setup do
+    must_be_root
+  end
+
+  met? {
+    "/etc/apt/sources.list.d/pgdg.list".p.exist?
+  }
+
+  meet do
+    shell 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+		shell 'wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -'
+  end
+
+  after {
+    shell "apt-get update"
+  }
+end
+
 dep 'pg.server' do
 	requires [
 		'postgresql-9.4.managed',
