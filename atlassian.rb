647f1c5c9d558bb3e803243c7665b799c9fc9d9f
Michael Shimmins <michael@teamsquare.co>
2016-01-07 21:28:26 +1100

    Extracted JIRA to common atlassian module

--- atlassian.rb
+++ atlassian.rb
@@ -1,3 +1,51 @@
+dep 'atlassian.product', :product_name, :version, :install_prefix, :home_directory, :username, :jdk_version do
+  home_directory.default!("/etc/#{product_name}")
+  username.default!('atlassian')
+  jdk_version.default!(7)
+
+  requires [
+    'jdk'.with(jdk_version),
+    'atlassian.user_exists'.with(username),
+    'atlassian.product.installed'.with(:product_name, :version, :install_prefix)
+  ]
+end
+
+dep 'atlassian.product.installed', :product_name, :version, :install_prefix, :remote_file_name do
+  install_prefix.default!('/usr/local/atlassian')
+  remote_file_name.default!("atlassian-#{product_name}-#{version}.tar.gz")
+
+  setup do
+    must_be_root
+  end
+
+  met? do
+    "#{install_prefix}/#{product_name}/bin/startup.sh".p.exists?
+  end
+
+  meet do
+    shell "mkdir -p #{install_prefix}"
+    shell "wget https://www.atlassian.com/software/#{product_name}/downloads/binary/#{remote_file_name} -P /tmp"
+    shell "tar xvf /tmp/#{tar_file} -C #{install_prefix}"
+    shell "mv #{install_prefix}/*#{product_name}* #{install_prefix}/#{product_name}"
+    shell "rm /tmp/#{tar_file}"
+  end
+end
+
+dep 'atlassian.product.home_directory_set', :product_name, :install_prefix, :home_directory do
+  setup do
+    must_be_root
+  end
+
+  met? do
+    "#{install_prefix}/#{product_name}/atlassian-#{product_name}/WEB-INF/classes/#{product_name}-application.properties".p.grep(/#{home_directory}/)
+  end
+
+  meet do
+    shell "mkdir -p #{home_directory}"
+    shell "echo '#{product_name}.home=#{home_directory}' > #{install_prefix}/#{product_name}/atlassian-jira/WEB-INF/classes/#{product_name}-application.properties"
+  end
+end
+
 dep 'atlassian.permissions', :install_prefix, :home_directory, :app_name, :username do
   met? do
     output = shell?("stat #{install_prefix}/#{app_name} | grep Uid | grep #{username}")
@@ -11,6 +59,9 @@ dep 'atlassian.permissions', :install_prefix, :home_directory, :app_name, :usern
   end
 end
 
-dep 'atlassian.user_exists' do
-  requires 'user and group exist'.with('atlassian', 'atlassian')
+dep 'atlassian.user_exists', :user_name, :group_name do
+  user_name.default!('atlassian')
+  group_name.default!(user_name)
+
+  requires 'user and group exist'.with(user_name, group_name)
 end
--- jira.rb
+++ jira.rb
@@ -1,48 +1,13 @@
-dep 'jira.installed', :version, :install_prefix do
+dep 'jira', :version, :install_prefix, :home_directory do
   version.default!('6.1.2')
   install_prefix.default!('/usr/local/atlassian')
-
-  setup do
-    must_be_root
-  end
-
-  met? do
-    "#{install_prefix}/jira/bin/startup.sh".p.exists?
-  end
-
-  meet do
-    shell "mkdir -p #{install_prefix}"
-    tar_file = "atlassian-jira-#{version}.tar.gz"
-    shell "wget https://www.atlassian.com/software/jira/downloads/binary/#{tar_file} -P /tmp"
-    shell "tar xvf /tmp/#{tar_file} -C #{install_prefix}"
-    shell "mv #{install_prefix}/*jira* #{install_prefix}/jira"
-    shell "rm /tmp/#{tar_file}"
-  end
-end
-
-dep 'jira', :version, :install_prefix, :home_directory do
   home_directory.default!('/etc/jira')
 
   requires [
                'jdk'.with(7),
                'atlassian.user_exists',
-               'jira.installed'.with(version, install_prefix),
-               'jira.home_directory_set'.with(install_prefix, home_directory),
+               'atlassian.product.installed'.with('jira', version, install_prefix, "atlassian-jira-software-#{version}-jira-#{version}.tar.gz"),
+               'atlassian.product.home_directory_set'.with('jira', install_prefix, home_directory),
                'atlassian.permissions'.with(install_prefix, home_directory, 'jira', 'atlassian')
            ]
-end
-
-dep 'jira.home_directory_set', :install_prefix, :home_directory do
-  setup do
-    must_be_root
-  end
-
-  met? do
-    "#{install_prefix}/jira/atlassian-jira/WEB-INF/classes/jira-application.properties".p.grep(/#{home_directory}/)
-  end
-
-  meet do
-    shell "mkdir -p #{home_directory}"
-    shell "echo 'jira.home=#{home_directory}' > #{install_prefix}/jira/atlassian-jira/WEB-INF/classes/jira-application.properties"
-  end
 end
