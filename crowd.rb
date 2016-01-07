2c74656db437a0a281c325487780584d9eff41b5
Michael Shimmins <michael@teamsquare.co>
2016-01-07 21:29:57 +1100

    Extracted crowd to common atlassian

--- crowd.rb
+++ crowd.rb
@@ -1,49 +1,15 @@
-dep 'crowd.installed', :version, :install_prefix do
+dep 'crowd', :version, :install_prefix, :home_directory do
   version.default!('2.8.3')
   install_prefix.default!('/usr/local/atlassian')
-
-  setup do
-    must_be_root
-  end
-
-  met? do
-    "#{install_prefix}/crowd/start_crowd.sh".p.exists?
-  end
-
-  meet do
-    shell "mkdir -p #{install_prefix}"
-    tar_file = "atlassian-crowd-#{version}.tar.gz"
-    shell "wget https://www.atlassian.com/software/crowd/downloads/binary/#{tar_file} -P /tmp"
-    shell "tar xvf /tmp/#{tar_file} -C #{install_prefix}"
-    shell "mv #{install_prefix}/*crowd* #{install_prefix}/crowd"
-    shell "rm /tmp/#{tar_file}"
-  end
-end
-
-dep 'crowd.home_directory_set', :install_prefix, :home_directory do
-  setup do
-    must_be_root
-  end
-
-  met? do
-    "#{install_prefix}/crowd/crowd-webapp/WEB-INF/classes/crowd-init.properties".p.grep(/#{home_directory}/)
-  end
-
-  meet do
-    shell "mkdir -p #{home_directory}"
-    shell "echo 'crowd.home=#{home_directory}' > #{install_prefix}/crowd/crowd-webapp/WEB-INF/classes/crowd-init.properties"
-  end
-end
-
-dep 'crowd', :version, :install_prefix, :home_directory do
   home_directory.default!('/etc/crowd')
 
   requires [
                'jdk'.with(7),
-               'crowd.installed'.with(version, install_prefix),
-               'crowd.home_directory_set'.with(install_prefix, home_directory),
+               'atlassian.user_exists',
+               'atlassian.product.installed'.with('crowd', version, install_prefix),
+               'atlassian.product.home_directory_set'.with('crowd', install_prefix, home_directory),
                'atlassian.permissions'.with(install_prefix, home_directory, 'crowd', 'atlassian')
-  ]
+           ]
 end
 
 dep 'crowd.startable', :version, :install_prefix do
