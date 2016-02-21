0e36ad109a6f02e25b0f8e91eecb8214a197dbb3
Michael Shimmins <michael@teamsquare.co>
2016-02-22 10:34:01 +1100

    Added default username for atlassian

--- atlassian.rb
+++ atlassian.rb
@@ -46,6 +46,8 @@ dep 'atlassian.product.home_directory_set', :product_name, :install_prefix, :hom
 end
 
 dep 'atlassian.permissions', :install_prefix, :home_directory, :app_name, :username do
+  username.default!('atlassian')
+  
   met? do
     output = shell?("stat #{install_prefix}/#{app_name} | grep Uid | grep #{username}")
 
