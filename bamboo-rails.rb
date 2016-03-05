32f7959492c9636e695510fe54d078a1f94c83e6
Michael Shimmins <michael@teamsquare.co>
2016-03-05 19:26:03 +1100

    Added nginx to bamboo setup

--- bamboo-rails.rb
+++ bamboo-rails.rb
@@ -1,5 +1,6 @@
 dep 'bamboo.rails', :version, :install_prefix, :home_directory do
   requires [
+    'nginx.auto_start',
     'bamboo.installed',
     'build essential installed',
     'nodejs.src',
