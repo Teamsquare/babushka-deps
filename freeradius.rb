a70d7103d4902414a68378a8a2a7c1c706143ba0
Katherine Shimmins <kath@teamsquare.co>
2016-05-12 11:13:10 +1000

    Added must be root guard to free radius installer

--- freeradius.rb
+++ freeradius.rb
@@ -1,4 +1,8 @@
 dep 'freeradius.src', :version do
+  setup do
+    must_be_root
+  end
+
   version.default!('3.0.11')
 
   source "ftp://ftp.freeradius.org/pub/freeradius//freeradius-server-#{version}.tar.gz"
