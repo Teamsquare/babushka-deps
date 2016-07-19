10b6fb60e32aef35e2f269e3d1d899343d7b3eac
Michael Shimmins <michael@teamsquare.co>
2016-07-19 20:15:53 +1000

    Updated URL for JIRA to new location Atlassian is using

--- jira.rb
+++ jira.rb
@@ -6,7 +6,7 @@ dep 'jira', :version, :install_prefix, :home_directory do
   requires [
                'jdk',
                'atlassian.user_exists',
-               'atlassian.product.installed'.with('jira', version, install_prefix, "atlassian-jira-software-#{version}-jira-#{version}.tar.gz"),
+               'atlassian.product.installed'.with('jira', version, install_prefix, "atlassian-jira-software-#{version}.tar.gz"),
                'atlassian.product.home_directory_set'.with('jira', install_prefix, home_directory),
                'atlassian.permissions'.with(install_prefix, home_directory, 'jira', 'atlassian')
            ]
