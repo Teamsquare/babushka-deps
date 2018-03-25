b9aee1f837535ddc164cc1704aef51c36c9dccd1
Michael Shimmins <shimms@users.noreply.github.com>
2018-03-25 13:47:50 +1100

    Fixed patch level

--- ruby.rb
+++ ruby.rb
@@ -1,12 +1,13 @@
-dep 'ruby.src', :version do
+dep 'ruby.src', :version, :patchlevel do
   def version_group
     version.to_s.scan(/^\d\.\d/).first
   end
 
   version.default!('2.4.3')
+  patchlevel.default!('p205')
   requires 'readline headers.managed', 'yaml headers.managed'
   source "https://cache.ruby-lang.org/pub/ruby/#{version_group}/ruby-#{version}.tar.gz"
-  provides "ruby == #{version}", 'gem', 'irb'
+  provides "ruby == #{version}#{patchlevel}", 'gem', 'irb'
   configure_args '--disable-install-doc',
                  "--with-readline-dir=#{Babushka.host.pkg_helper.prefix}",
                  "--with-libyaml-dir=#{Babushka.host.pkg_helper.prefix}"
